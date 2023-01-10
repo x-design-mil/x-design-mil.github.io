f2f1d6b3825260aae59273a70f4c64e1b41e02ae
Raffaele Amietta <70523797+rahffaele@users.noreply.github.com>
2023-01-10 12:29:06 +0100

    js

---
+++ script.js
@@ -0,0 +1,34 @@
+const timer = {
+  pomodoro: 25,
+  shortBreak: 5,
+  longBreak: 15,
+  longBreakInterval: 4,
+};
+
+const modeButtons = document.querySelector('#button');
+modeButtons.addEventListener('click', handleMode);
+
+function handleMode(event) {
+  const { mode } = event.target.dataset;
+
+  if (!mode) return;
+
+  switchMode(mode);
+}
+
+function switchMode(mode) {
+  timer.mode = mode;
+  timer.remainingTime = {
+    total: timer[mode] * 60,
+    minutes: timer[mode],
+    seconds: 0,
+  };
+
+  document
+    .querySelectorAll('button[data-mode]')
+    .forEach(e => e.classList.remove('active'));
+  document.querySelector(`[data-mode="${mode}"]`).classList.add('active');
+  document.body.style.backgroundColor = `var(--${mode})`;
+
+  updateClock();
+}
